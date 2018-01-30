22241568f17bca1171c254b70e61ac208689461a
sakib04 <md.nazmussakib03@gmail.com>
2018-01-30 19:18:22 +0600

    first file upload

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+
+    using System;
+        class HelloWorld
+        {
+            static void Main()
+            {
+                // Use the system console object
+                Console.WriteLine("Hello World");
+                Console.WriteLine();
+            }
+         }
+        
+
+        
+    
+}
---
+++ ConsoleApplication2/value.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace value
+{
+
+    //class Values
+    //{
+    //    static void Main()
+    //    {
+    //        int myInt;
+    //        myInt = 7;
+    //        System.Console.WriteLine("Assigned, myInt: {0}", myInt);
+           
+    //        myInt = 5;
+    //        System.Console.WriteLine("Reassigned, myInt: {0}", myInt);
+
+    //        System.Console.ReadLine();
+    //    }
+    //}
+    //const int FreezingPoint = 32;
+       class Values
+                    {
+                        static void Main( )
+                                            {
+                                                int myInt = 7;
+                                                System.Console.WriteLine("Initialized, myInt: {0}",myInt);
+                                                myInt = 5;
+                                                System.Console.WriteLine("After assignment, myInt: {0}",myInt);
+                                                System.Console.ReadLine();
+                                            }
+                    }
+}
+        
+
+        
+    
+
