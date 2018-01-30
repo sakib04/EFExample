4cbeb509f46d742a65450083d598e7e86680a3e3
sakib04 <md.nazmussakib03@gmail.com>
2018-01-30 19:29:54 +0600

    student property add

--- ConsoleApplication2/Models/student.cs
+++ ConsoleApplication2/Models/student.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication2
 {
-    class Student
+    public class Student
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string reg { get; set; }
     }
 }
